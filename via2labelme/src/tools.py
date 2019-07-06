4669e9005181389fba9c70d54a526fa1935203ce
I302861 <samir.sheriff@sap.com>
2019-07-06 18:04:58 +0530

    Fixed runtime errors and added support to specify a label_mapper function

--- via2labelme/src/tools.py
+++ via2labelme/src/tools.py
@@ -4,17 +4,16 @@ from pathlib import Path
 import matplotlib.image as mpimg
 from tqdm import tqdm
 import argparse
+import sys
 
 
-def get_pascal_json(via_json, path=Path('.')):
+def get_pascal_json(via_json, path=Path('.'), label_mapper=None):
     pascal_json = {}
     pascal_json['version'] = '3.16.1'
     pascal_json['flags'] = {}
-    pascal_json['shapes'] = get_pascal_shapes_json(via_json)
-    lineColor_json = [0, 255, 0, 128]
-    pascal_json['lineColor'] = lineColor_json
-    fillColor_json = [255, 0, 0, 128]
-    pascal_json['fillColor'] = fillColor_json
+    pascal_json['shapes'] = get_pascal_shapes_json(via_json, label_mapper)
+    pascal_json['lineColor'] = [0, 255, 0, 128]
+    pascal_json['fillColor'] = [255, 0, 0, 128]
     pascal_json['imagePath'] = via_json['filename']
     with open(path / via_json['filename'], 'rb') as f:
         pascal_json['imageData'] = f.read()
@@ -25,14 +24,25 @@ def get_pascal_json(via_json, path=Path('.')):
     return pascal_json
 
 
-def get_pascal_shapes_json(via_json):
+def get_region_attributes_label(via_region_attributes, label_mapper=None, filename='unknown'):
+    try:
+        if label_mapper is None:
+            return str(via_region_attributes)
+        else:
+            return label_mapper(via_region_attributes)
+    except Exception as e:
+        print('Unable to find label for', filename, 'with exception', e)
+        sys.exit(1)
+
+
+def get_pascal_shapes_json(via_json, label_mapper=None):
     pascal_shapes = []
     for region, attributes in via_json['regions'].items():
         via_shape_attributes = attributes['shape_attributes']
         via_region_attributes = attributes['region_attributes']
         pascal_shape = {}
         pascal_shape['shape_type'] = via_shape_attributes['name']
-        pascal_shape['label'] = via_region_attributes['anomaly']
+        pascal_shape['label'] = get_region_attributes_label(via_region_attributes, label_mapper, via_json['filename'])
         pascal_shape['line_color'] = None
         pascal_shape['fill_color'] = None
         pascal_shape['flags'] = {}
@@ -45,17 +55,17 @@ def get_pascal_shapes_json(via_json):
     return pascal_shapes
 
 
-def generate_pascal_json_file(via_json, path=Path('.')):
-    pascal_json = get_pascal_json(via_json, path)
+def generate_pascal_json_file(via_json, path=Path('.'), label_mapper=None):
+    pascal_json = get_pascal_json(via_json, path, label_mapper)
     json_filename = ''.join(via_json['filename'].split('.')[:-1] + ['.json'])
     with open(path / json_filename, 'w') as json_file:
         json.dump(pascal_json, json_file, indent=4)
 
 
-def generate_pascal_json_files(path=Path('.'), jsonPath=Path('.')):
+def generate_pascal_json_files(path=Path('.'), jsonPath=Path('.'), label_mapper=None):
     full_via_json = json.loads(open(jsonPath).read())
     for _, via_json in tqdm(full_via_json.items()):
-        generate_pascal_json_file(via_json, path)
+        generate_pascal_json_file(via_json, path, label_mapper)
 
 
 if __name__ == '__main__':
@@ -64,4 +74,4 @@ if __name__ == '__main__':
     parser.add_argument('inputdir', type=str, help='Path to Directory containing images annotated with the VIA tool')
     parser.add_argument('inputjson', type=str, help='Path to JSON file containing all annotations')
     args = parser.parse_args()
-    generate_pascal_json_files(args.inputdir, args.inputjson)
+    generate_pascal_json_files(Path(args.inputdir), Path(args.inputjson))
